58609c4787f2b39998a4d4642b44d4b5977d3c8f
Tatiana-SPB <taya.kremleva.95@gmail.com>
2026-04-04 13:58:44 +0300

    edits have been made to HW1

--- src/components/CardsItem.jsx
+++ src/components/CardsItem.jsx
@@ -1,24 +1,39 @@
-export function CardsItemWebDesign() {
+export const CardsItem = ({ text }) => {
+  const getThemeClass = () => {
+    switch (text) {
+      case "Research":
+        return "_green";
+      case "Web Design":
+        return "_orange";
+      case "Copywriting":
+        return "_purple";
+      default:
+        return "";
+    }
+  };
+
+  const themeClass = getThemeClass();
+
   return (
-    <div class="cards__item">
-      <div class="cards__card card">
-        <div class="card__group">
-          <div class="card__theme _orange">
-            <p class="_orange">Web Design</p>
+    <div className="cards__item">
+      <div className="cards__card card">
+        <div className="card__group">
+          <div className={`card__theme ${themeClass}`}>
+            <p className={themeClass}>{text}</p>
           </div>
           <a href="#popBrowse" target="_self">
-            <div class="card__btn">
+            <div className="card__btn">
               <div></div>
               <div></div>
               <div></div>
             </div>
           </a>
         </div>
-        <div class="card__content">
+        <div className="card__content">
           <a href="" target="_blank">
-            <h3 class="card__title">Название задачи</h3>
+            <h3 className="card__title">Название задачи</h3>
           </a>
-          <div class="card__date">
+          <div className="card__date">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="13"
@@ -26,19 +41,19 @@ export function CardsItemWebDesign() {
               viewBox="0 0 13 13"
               fill="none"
             >
-              <g clip-path="url(#clip0_1_415)">
+              <g clipPath="url(#clip0_1_415)">
                 <path
                   d="M10.5625 2.03125H2.4375C1.7644 2.03125 1.21875 2.5769 1.21875 3.25V10.5625C1.21875 11.2356 1.7644 11.7812 2.4375 11.7812H10.5625C11.2356 11.7812 11.7812 11.2356 11.7812 10.5625V3.25C11.7812 2.5769 11.2356 2.03125 10.5625 2.03125Z"
                   stroke="#94A6BE"
-                  stroke-width="0.8"
-                  stroke-linejoin="round"
+                  strokeWidth="0.8"
+                  strokeLinejoin="round"
                 />
                 <path
                   d="M11.7812 4.0625H1.21875M3.25 1.21875V2.03125V1.21875ZM9.75 1.21875V2.03125V1.21875Z"
                   stroke="#94A6BE"
-                  stroke-width="0.8"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="0.8"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 />
               </g>
               <defs>
@@ -53,4 +68,6 @@ export function CardsItemWebDesign() {
       </div>
     </div>
   );
-}
+};
+
+export default CardsItem;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,25 @@
-import { PopUser } from "./popups/PopUserComponents";
+import { PopUser } from "./popups/PopUser";
 
 export function Header() {
   return (
-    <header class="header">
-      <div class="container">
-        <div class="header__block">
-          <div class="header__logo _show _light">
+    <header className="header">
+      <div className="container">
+        <div className="header__block">
+          <div className="header__logo _show _light">
             <a href="" target="_self">
               <img src="images/logo.png" alt="logo" />
             </a>
           </div>
-          <div class="header__logo _dark">
+          <div className="header__logo _dark">
             <a href="" target="_self">
               <img src="images/logo_dark.png" alt="logo" />
             </a>
           </div>
-          <nav class="header__nav">
-            <button class="header__btn-main-new _hover01" id="btnMainNew">
+          <nav className="header__nav">
+            <button className="header__btn-main-new _hover01" id="btnMainNew">
               <a href="#popNewCard">Создать новую задачу</a>
             </button>
-            <a href="#user-set-target" class="header__user _hover02">
+            <a href="#user-set-target" className="header__user _hover02">
               Ivan Ivanov
             </a>
 

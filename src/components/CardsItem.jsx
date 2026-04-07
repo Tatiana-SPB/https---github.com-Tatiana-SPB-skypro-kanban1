7f1aaccc09fe929c0c5f628ca5380fb85031a311
Tatiana-SPB <taya.kremleva.95@gmail.com>
2026-04-07 23:46:01 +0300

    completed hw2

--- src/components/CardsItem.jsx
+++ src/components/CardsItem.jsx
@@ -1,73 +1,124 @@
-export const CardsItem = ({ text }) => {
-  const getThemeClass = () => {
-    switch (text) {
-      case "Research":
-        return "_green";
-      case "Web Design":
-        return "_orange";
-      case "Copywriting":
-        return "_purple";
-      default:
-        return "";
-    }
-  };
+import { cardList } from "../data";
+import { getThemeClass } from "./getThemeClass";
 
-  const themeClass = getThemeClass();
+export const CardsItem = ({ status }) => {
+  const filteredCards = cardList.filter((card) => card.status === status);
 
-  return (
-    <div className="cards__item">
-      <div className="cards__card card">
-        <div className="card__group">
-          <div className={`card__theme ${themeClass}`}>
-            <p className={themeClass}>{text}</p>
-          </div>
-          <a href="#popBrowse" target="_self">
-            <div className="card__btn">
-              <div></div>
-              <div></div>
-              <div></div>
+  if (filteredCards.length > 0) {
+    return (
+      <>
+        {filteredCards.map((cardEl) => (
+          <div className="cards__item" key={cardEl.id}>
+            <div className="cards__card card">
+              <div className="card__group">
+                <div className={`card__theme ${getThemeClass(cardEl.topic)}`}>
+                  <p className={getThemeClass(cardEl.topic)}>{cardEl.topic}</p>
+                </div>
+                <a href="#popBrowse" target="_self">
+                  <div className="card__btn">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                  </div>
+                </a>
+              </div>
+              <div className="card__content">
+                <a href="" target="_blank">
+                  <h3 className="card__title">{cardEl.title}</h3>
+                </a>
+                <div className="card__date">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="13"
+                    height="13"
+                    viewBox="0 0 13 13"
+                    fill="none"
+                  >
+                    <g clipPath="url(#clip0_1_415)">
+                      <path
+                        d="M10.5625 2.03125H2.4375C1.7644 2.03125 1.21875 2.5769 1.21875 3.25V10.5625C1.21875 11.2356 1.7644 11.7812 2.4375 11.7812H10.5625C11.2356 11.7812 11.7812 11.2356 11.7812 10.5625V3.25C11.7812 2.5769 11.2356 2.03125 10.5625 2.03125Z"
+                        stroke="#94A6BE"
+                        strokeWidth="0.8"
+                        strokeLinejoin="round"
+                      />
+                      <path
+                        d="M11.7812 4.0625H1.21875M3.25 1.21875V2.03125V1.21875ZM9.75 1.21875V2.03125V1.21875Z"
+                        stroke="#94A6BE"
+                        strokeWidth="0.8"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </g>
+                    <defs>
+                      <clipPath id="clip0_1_415">
+                        <rect width="13" height="13" fill="white" />
+                      </clipPath>
+                    </defs>
+                  </svg>
+                  <p>{cardEl.date}</p>
+                </div>
+              </div>
             </div>
-          </a>
-        </div>
-        <div className="card__content">
-          <a href="" target="_blank">
-            <h3 className="card__title">Название задачи</h3>
-          </a>
-          <div className="card__date">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="13"
-              height="13"
-              viewBox="0 0 13 13"
-              fill="none"
-            >
-              <g clipPath="url(#clip0_1_415)">
-                <path
-                  d="M10.5625 2.03125H2.4375C1.7644 2.03125 1.21875 2.5769 1.21875 3.25V10.5625C1.21875 11.2356 1.7644 11.7812 2.4375 11.7812H10.5625C11.2356 11.7812 11.7812 11.2356 11.7812 10.5625V3.25C11.7812 2.5769 11.2356 2.03125 10.5625 2.03125Z"
-                  stroke="#94A6BE"
-                  strokeWidth="0.8"
-                  strokeLinejoin="round"
-                />
-                <path
-                  d="M11.7812 4.0625H1.21875M3.25 1.21875V2.03125V1.21875ZM9.75 1.21875V2.03125V1.21875Z"
-                  stroke="#94A6BE"
-                  strokeWidth="0.8"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </g>
-              <defs>
-                <clipPath id="clip0_1_415">
-                  <rect width="13" height="13" fill="white" />
-                </clipPath>
-              </defs>
-            </svg>
-            <p>30.10.23</p>
+          </div>
+        ))}
+      </>
+    );
+  } else {
+    return (
+      <div className="cards__item">
+        <div className="cards__card card">
+          <div className="card__group">
+            <div className="card__theme">
+              <p></p>
+            </div>
+            <a href="#popBrowse" target="_self">
+              <div className="card__btn">
+                <div></div>
+                <div></div>
+                <div></div>
+              </div>
+            </a>
+          </div>
+          <div className="card__content">
+            <a href="" target="_blank">
+              <h3 className="card__title">Нет задач</h3>
+            </a>
+            <div className="card__date">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="13"
+                height="13"
+                viewBox="0 0 13 13"
+                fill="none"
+              >
+                <g clipPath="url(#clip0_1_415)">
+                  <path
+                    d="M10.5625 2.03125H2.4375C1.7644 2.03125 1.21875 2.5769 1.21875 3.25V10.5625C1.21875 11.2356 1.7644 11.7812 2.4375 11.7812H10.5625C11.2356 11.7812 11.7812 11.2356 11.7812 10.5625V3.25C11.7812 2.5769 11.2356 2.03125 10.5625 2.03125Z"
+                    stroke="#94A6BE"
+                    strokeWidth="0.8"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M11.7812 4.0625H1.21875M3.25 1.21875V2.03125V1.21875ZM9.75 1.21875V2.03125V1.21875Z"
+                    stroke="#94A6BE"
+                    strokeWidth="0.8"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </g>
+                <defs>
+                  <clipPath id="clip0_1_415">
+                    <rect width="13" height="13" fill="white" />
+                  </clipPath>
+                </defs>
+              </svg>
+              <p></p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default CardsItem;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,33 @@
 import { Column } from "./Column";
 
-export function Main() {
-  return (
+export function Main({ loading }) {
+  return loading ? (
+    <div
+      style={{
+        color: "green",
+        fontSize: "20px",
+        fontWeight: 600,
+        lineHeight: 1,
+        textAlign: "center",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+      }}
+    >
+      Данные загружаются
+    </div>
+  ) : (
     <main className="main">
       <div className="container">
         <div className="main__block">
-          <Column />
+          <div className="main__content">
+            <Column status="Без статуса" />
+            <Column status="Нужно сделать" />
+            <Column status="В работе" />
+            <Column status="Тестирование" />
+            <Column status="Готово" />
+          </div>
         </div>
       </div>
     </main>
